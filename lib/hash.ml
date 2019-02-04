9bdc57a8daefb2478ff710741dd5da737c0e95da
Ryo Hashiguchi <ryo12redstone@gmail.com>
2019-02-04 21:41:52 +0900

    Make Hash.hash returns Hash.t

--- lib/hash.ml
+++ lib/hash.ml
@@ -4,17 +4,18 @@ open Bos
 
 type t = { algorithm : HashAlgorithm.t; hash : string }
 
-let hash hashAlgorithm text =
+let hash algorithm text =
   let unwrap = function
   | Ok result ->
-    String.chop_prefix_exn ~prefix:"(stdin)= " result
+    let hex = String.chop_prefix_exn ~prefix:"(stdin)= " result in
+    Hex.to_string (`Hex hex)
   | _ -> raise (Failure "") in
-  match hashAlgorithm with
+  match algorithm with
   | Sha1 ->
     let cmd = Cmd.(v "openssl" % "dgst" % "-sha1" % "-hex") in
     let result = OS.Cmd.(run_io cmd (in_string text) |> to_string) in
-    unwrap result
+    { algorithm = algorithm; hash = unwrap result }
   | Sha256 ->
     let cmd = Cmd.(v "openssl" % "dgst" % "-sha256" % "-hex") in
     let result = OS.Cmd.(run_io cmd (in_string text) |> to_string) in
-    unwrap result
+    { algorithm = algorithm; hash = unwrap result }
--- test/hash_test.ml
+++ test/hash_test.ml
@@ -1,19 +1,31 @@
 open YayakaChain
 module Test = Alcotest
 
+let pp ppf = function
+  | Hash.{ algorithm = Sha1; hash } -> Fmt.pf ppf "Sha1 %s" (Hex.(of_string hash |> show))
+  | Hash.{ algorithm = Sha256; hash } -> Fmt.pf ppf "Sha256 %s" (Hex.(of_string hash |> show))
+
+let hash = Test.testable pp ( = )
+
+let h hex = Hex.to_string (`Hex hex)
+
 let test_hash () =
-  Test.(check string) "a with sha1"
-    "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8"
-    (Hash.hash HashAlgorithm.Sha1 "a");
-  Test.(check string) "abc with sha1"
-    "a9993e364706816aba3e25717850c26c9cd0d89d"
-    (Hash.hash HashAlgorithm.Sha1 "abc");
-  Test.(check string) "a with sha256"
-    "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb"
-    (Hash.hash HashAlgorithm.Sha256 "a");
-  Test.(check string) "abc with sha256"
-    "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
-    (Hash.hash HashAlgorithm.Sha256 "abc")
+  Test.(check hash) "a with sha1" {
+    algorithm = Sha1;
+    hash = (h "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8") }
+    (Hash.hash Sha1 "a");
+  Test.(check hash) "abc with sha1" {
+    algorithm = Sha1;
+    hash = (h "a9993e364706816aba3e25717850c26c9cd0d89d") }
+    (Hash.hash Sha1 "abc");
+  Test.(check hash) "a with sha256" {
+    algorithm = Sha256;
+    hash = (h "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb") }
+    (Hash.hash Sha256 "a");
+  Test.(check hash) "abc with sha256" {
+    algorithm = Sha256;
+    hash = (h "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad") }
+    (Hash.hash Sha256 "abc")
 
 let hash_tests = [
   "hash", `Quick, test_hash;
